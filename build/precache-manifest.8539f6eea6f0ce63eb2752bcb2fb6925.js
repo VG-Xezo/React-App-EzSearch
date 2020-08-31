a38798ad5c78a967cd1eb7411c80f2c13e0186ec
HACKZ1 <43455889+HACKZ1@users.noreply.github.com>
2020-08-31 14:43:14 -0400

    Add files via upload

---
+++ build/precache-manifest.8539f6eea6f0ce63eb2752bcb2fb6925.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98e353986b6c4d543692690ec21a61a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e295421a0edfbaa8dd75",
+    "url": "/static/css/main.db751f76.chunk.css"
+  },
+  {
+    "revision": "62545da5d9668f3b6f00",
+    "url": "/static/js/2.e044d0f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e044d0f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e295421a0edfbaa8dd75",
+    "url": "/static/js/main.f691ab77.chunk.js"
+  },
+  {
+    "revision": "2eadf85e86b83972d00f",
+    "url": "/static/js/runtime-main.8d454dc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
